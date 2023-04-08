5f3d21ed5ef437b505a631c923ebf3199a4728cb
shivank-dvlpr <shivankyadav1234@gmail.com>
2023-04-08 12:10:29 +0530

    Added Info Activity

---
+++ app/src/main/java/com/shivank/gdsubway_gla/InfoClass.java
@@ -0,0 +1,14 @@
+package com.shivank.gdsubway_gla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.infoclass_activity);
+    }
+}
--- app/src/main/java/com/shivank/gdsubway_gla/MainActivity.java
+++ app/src/main/java/com/shivank/gdsubway_gla/MainActivity.java
@@ -2,15 +2,38 @@ package com.shivank.gdsubway_gla;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView imgInfo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setTitle("GD Subway");
+        getSupportActionBar().hide();
+
+        imgInfo = findViewById(R.id.imgInfo);
+        imgInfo.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+
+            case R.id.imgInfo:
+                startActivity(new Intent(MainActivity.this, InfoClass.class));
+                break;
+
+        }
 
     }
 }
